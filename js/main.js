6db84fcfe0300b693716684ee518ffe09e82d9fb
¨luciano <lucianopettit@gmail.com>
2023-02-18 20:17:44 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
-
 //  impuestos arg (iva+impuestos a todo xD)
-const tasaImpuestos = 0.75;
+const tasaImpuestos = 0.76;
 
 // precio del juego (tal cual sale en steam)
 let precioBase = Number(
   prompt("Ingresa el precio base del juego en Steam (sin impuestos):")
 );
 
-// verifica si el valor que puso es valido o mayor que 0 
+// verifica si el valor que puso es valido o mayor que 0
 while (isNaN(precioBase) || precioBase <= 0) {
   precioBase = Number(
     prompt("El precio base debe ser mayor que cero. Ingresa un precio válido:")
   );
 }
 
-// Calcula el impuesto es decir mutiplica el valor del juego por la tasa de impuestos 
+// Calcula el impuesto es decir mutiplica el valor del juego por la tasa de impuestos
 const impuesto = precioBase * tasaImpuestos;
 
 // Calcula el precio total con impuestos es decir que suma el precio del juego mas el resultado de lo impuestos
 const precioTotal = precioBase + impuesto;
 
-// Imprime el resultado del valor del juego mas impuestos  
+// Imprime el resultado del valor del juego mas impuestos
 alert(`El precio total del juego con impuestos es: ${precioTotal}`);
 
 // Verifica si el precio total es mayor que $7000
